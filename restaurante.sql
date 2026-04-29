225ea1e22429420f76c6178252e34a0aae3e42d6
amarzam2303 <amarzam2303@g.educaand.es>
2026-04-29 22:26:49 +0200

    CUARTO COMMIT

--- restaurante.sql
+++ restaurante.sql
@@ -4,19 +4,19 @@ USE restaurante;
 
 --Creación de la tabla menú
 CREATE TABLE Menu (
-    tipo NVARCHAR(255),
-    nombre NVARCHAR(255) NOT NULL,
-    descripcion NVARCHAR(255) NOT NULL,
+    tipo VARCHAR(255),
+    nombre VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
     CONSTRAINT pk_menu_tipo PRIMARY KEY(tipo)
 );
 
 --Creación de la tabla de bebidas
 CREATE TABLE Bebida (
-    nombre NVARCHAR(255) NOT NULL,
-    pais_origen NVARCHAR(255) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    pais_origen VARCHAR(255) NOT NULL,
     precio FLOAT NOT NULL,
-    temperatura NVARCHAR (255) NOT NULL,
-    con_o_sin_hielo NVARCHAR (255) NOT NULL,
+    temperatura VARCHAR (255) NOT NULL,
+    con_o_sin_hielo VARCHAR (255) NOT NULL,
     CONSTRAINT pk_bebida_nombre PRIMARY KEY (nombre),
     CONSTRAINT ch_bebida_temperatura CHECK (temperatura IN('Fría', 'Del tiempo', 'Caliente')),
     CONSTRAINT ch_bebida_con_o_sin_hielo CHECK (con_o_sin_hielo IN('Con hielo', 'Sin hielo'))
@@ -24,18 +24,18 @@ CREATE TABLE Bebida (
 
 --Creación de la tabla de platos principales
 CREATE TABLE Plato_principal (
-    nombre NVARCHAR(255) NOT NULL,
-    pais_origen NVARCHAR(255) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    pais_origen VARCHAR(255) NOT NULL,
     precio FLOAT NOT NULL,
     CONSTRAINT pk_plato_principal_nombre PRIMARY KEY (nombre)
 );
 
 --Creación de la tabla de postres
 CREATE TABLE Postre (
-    nombre NVARCHAR(255) NOT NULL,
-    pais_origen NVARCHAR(255) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    pais_origen VARCHAR(255) NOT NULL,
     precio FLOAT NOT NULL,
-    con_o_sin_azucar NVARCHAR (255) NOT NULL,
+    con_o_sin_azucar VARCHAR (255) NOT NULL,
     CONSTRAINT pk_postre_nombre PRIMARY KEY (nombre),
     CONSTRAINT ch_postre_con_o_sin_azucar CHECK (con_o_sin_azucar IN('Con azúcar', 'Sin azúcar'))
 );
@@ -43,8 +43,8 @@ CREATE TABLE Postre (
 --Creación de la tabla de ingredientes
 CREATE TABLE Ingrediente (
     id IDENTITY(1,1),
-    nombre NVARCHAR(255) NOT NULL,
-    tipo NVARCHAR(255) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    tipo VARCHAR(255) NOT NULL,
     CONSTRAINT pk_ingrediente_id PRIMARY KEY (id),
     CONSTRAINT ch_ingrediente_tipo CHECK (tipo IN('Proteínas', 'Carbohidratos/Almidones', 'Grasas/Lácteos', 'Verduras y Frutas', 'Condimentos/Salsas/Especias'))
 );

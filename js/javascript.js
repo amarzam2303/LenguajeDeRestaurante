a8dd7ee2d4bf032fb88b6b80a08e647a0fb65a93
amarzam2303 <amarzam2303@g.educaand.es>
2026-04-29 20:33:31 +0200

    tercer commit

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+const PORT = 3000; // Puerto donde correrá nuestro servidor
+const ENDPOINT_SERVER = "http://localhost"; // URL base de nuestro servidor
+const ENDPOINT_OBTENER_MENU = "usuarios";
+const ENDPOINT_INSERTAR_ELIMINAR_ACTUALIZAR_MENU = "usuario";
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    
+});
